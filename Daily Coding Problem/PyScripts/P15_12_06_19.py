e655879f789c23838a56764f0c15f9195d8a52cd
Lenovo <Lenovo@DESKTOP-C54NBA8.esec.coriolis.in>
2019-06-25 14:39:27 +0530

    New Scripts

---
+++ Daily Coding Problem/PyScripts/P15_12_06_19.py
@@ -0,0 +1,8 @@
+'''
+This problem was asked by Facebook.
+Given a stream of elements too large to store in memory, pick a random element from the stream with uniform probability.
+
+Created on 25-Jun-2019
+
+@author: Lenovo
+'''
